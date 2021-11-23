1a6fa943ccbc54337ed5303d1f1d811c9901aba5
kovacs.richard90@gmail.com <dev.kovacs.richard90@gmail.com>
2021-11-23 20:03:40 +0100

    write meta data

--- utils/search.py
+++ utils/search.py
@@ -3,11 +3,17 @@
     # képeket -> pipa
     # szöveges állományt -> json -> pipa
     # még egyéb metaadatokat
+    # a képeket egy posters nevű mappába kiírni
+    # a json-öket kiírni egy meta_data mappába
     # ha mégiscsak a listában van az elem, akkor próbáljuk megtalálni a megfelelőt
+
 # később refakotorálnoi fogjuk a kódot: python loggert fogunk használni
 
+import os
 import tmdbsimple as tmdb
 from urllib.request import urlopen
+import file_handler
+
 
 class Search:
     tmdb.API_KEY = '454b6ca4172e455fe7a7d8395c10d6d9'
@@ -15,9 +21,12 @@ class Search:
     backdrop_path_string = 'https://image.tmdb.org/t/p/original'
 
     def __init__(self):
-        pass
+        self.file_handler = file_handler.FileHandler()
+        self.movie_name = None
 
+    # setter és getter megoldásra fogjuk majd cserélni
     def get_json_data(self, movie_name):
+        self.movie_name = movie_name
         if not movie_name:
             raise Exception("Nem adtál meg film címet, kérlek pótold")
         try:
@@ -32,34 +41,38 @@ class Search:
     def _get_poster_link(self, data):
         return f"{self.image_path_string}{data['poster_path']}"
         
-    def write_image(self, data, movie_name):
+    def write_image(self, data):
         if not data:
             return False, "There is no data"
 
         poster_link = self._get_poster_link(data)
 
         try:
-            with open(movie_name + '.jpg', "wb") as poster:
+            with open(os.path.join(self.file_handler.poster_folder, self.movie_name + '.jpg'), "wb") as poster:
                 poster.write(urlopen(poster_link).read())
         except Exception as e:
             return False, str(e)
 
+    def write_meta_data(self, data):
+        if not os.path.exists(self.file_handler.meta_data_folder):
+            return False, f"The given {self.file_handler.meta_data_folder} folder does not exists"
+
+        file_path = os.path.join(self.file_handler.meta_data_folder, self.movie_name + '.json')
+        return self.file_handler.write_file(file_path, data)
 
 if __name__ == '__main__':
     test = Search()
 
-    #print(test.image_path_string)
-
     movie_name = 'Braveheart'
 
-    data = test.get_json_data(movie_name)
+    movies = [movie[:-4] for movie in os.listdir(r"C:\WORK\Prooktatás\oop_project\movies") if '.mkv' in movie]
+    print(movies)
+    for item in movies:
+        data = test.get_json_data(item)
+        test.write_meta_data(data)
 
-    print(data)
+        if not data:
+            print(f"error at query data from api: {data}")
+            exit()
 
-    if not data:
-        print(f"error at query data from api: {data}")
-        exit()
-
-    test.write_image(data, movie_name)
-
-    
+        test.write_image(data)
