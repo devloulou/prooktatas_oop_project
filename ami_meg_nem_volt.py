7a8b2c633049a142061c48ee7f0178583ac77a2e
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-01-25 20:51:39 +0100

    ui for app

---
+++ ami_meg_nem_volt.py
@@ -0,0 +1,8 @@
+# Thread # Multiprocessing ~ 1 óra
+# Pandas # több féle file-kezelést ~ legalább 1 egész alkalom
+# Refakt + 1 feature: több mappa egyidejű kezelése ~ minimum fél alkalom
+# reqest modul használata -> http műveletek ~ 1 alkalom
+# logger pythonban ~ minimum 1 óra
+# unittest ~ minimum 1 óra
+
+# gyakorolni fogunk
