294b38951b6fe3dc91a583449dbcee02c6379423
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-02-03 17:22:49 +0100

    27_alkalom test feladat

---
+++ 27_alkalom_test.py
@@ -0,0 +1,18 @@
+# 4. feladat: Írjatok egy olyan programot, amely megmondja egy megadott szóról, hogy 
+# palindrome- e.
+# A pilindrome azt jelenti, hogy a szó  visszafelé olvasva ugyan azt a szót adja
+# pl. görög ->ez visszafelé olvasva is görög; 
+
+# 5. feladat: írjatok egy olyan programot, 
+# amely paraméterben megadott adatot kiírja egy megadott helyre
+# json-ben
+
+# 6. feladat: Írjatok egy olyan programot, amely a 4 matematikai alapmáveletet
+# összeadás, kivonás, szorzás, osztás elvégzi
+# a megoldás állhat több függvényből
+# törekedjetek a matematika szabályainak betartására
+
+# a futtatás meghívása a következő legyen:
+# pl. my_func(2, 3, "+") -> a művelet minden esetben string legyen
+
+# nem szükséges ennél komplexebb megvalósítás- több szám, kombinált műveletek
